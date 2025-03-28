10c6697eac2cbea4a61ad188141439756ebac325
DevinCoombs <mariorules.devin@gmail.com>
2025-03-28 08:43:34 -0600

    renames.

--- capstone/test/specs/brandsTest.e2e.js
+++ capstone/test/specs/brandsTest.e2e.js
@@ -1,7 +1,4 @@
-﻿const { expect } = require('@wdio/globals')
-const mainPage = require('../pageobjects/mainPage')
-const appliancePage = require('../pageobjects/appliancePage')
-const microwavePage = require('../pageobjects/microwavePage')
+﻿const mainPage = require('../pageobjects/mainPage')
 
 describe('Our brands', () => {
     it('Take you to correct page', async () => {
--- capstone/test/specs/socialMediaTest.e2e.js
+++ capstone/test/specs/socialMediaTest.e2e.js
@@ -1,7 +1,4 @@
-const { expect } = require('@wdio/globals')
 const mainPage = require('../pageobjects/mainPage')
-const appliancePage = require('../pageobjects/appliancePage')
-const microwavePage = require('../pageobjects/microwavePage')
 
 describe('Social Media', () => {
     it('Take you to correct social media site', async () => {
